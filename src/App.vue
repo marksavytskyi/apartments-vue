0053929dab5bb3570bbcf962b8fb4364abdfce98
Mark Savytskyi <107767178+marksavytskyi@users.noreply.github.com>
2023-10-08 15:29:48 -0700

    done

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from "vue";
+import ApartmentsListVue from "./components/Apartments/ApartmentsList.vue";
+import AptItems from "./components/Apartments/apartments";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <ApartmentsListVue :items="AptItems" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  flex-grow: 1;
+}
+</style>
---
+++ src/components/Apartments/ApartmentsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="apartments-list">
+    <ApartmentsItem
+      v-for="{id, imgUrl, descr, rating, price} in items"
+      :key="id"
+      :descr="descr"
+      imgSrc="/public/img/apartment-1.jpg"
+      :rating="rating"
+      :price="price"
+    />
+  </div>
+</template>
+
+<script>
+import ApartmentsItem from "./ApartmentsItem.vue";
+export default {
+  name: "ApartmentsList",
+  components: {ApartmentsItem},
+  props: {
+    items: Array,
+    default: () => [],
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.apartments-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
+}
+</style>
